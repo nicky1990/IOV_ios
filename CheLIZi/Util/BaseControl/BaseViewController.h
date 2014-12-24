11595a2cfc870db646b20d119077bb33b99dfce3
linqinbin <linqb@cheerzing.com>
2014-12-24 10:01:37 +0800

    增加了刷新控件，指示器，网络监测库和基础控制器
    Change-Id: I23f1af6eaa1f87c003e563177880319940ca6010

---
+++ CheLIZi/Util/BaseControl/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  CheLIZi
+//
+//  Created by 点睛石 on 14/12/24.
+//  Copyright (c) 2014年 骐俊通联. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ CheLIZi/Util/BaseControl/BaseViewController.m
@@ -0,0 +1,37 @@
+//
+//  BaseViewController.m
+//  CheLIZi
+//
+//  Created by 点睛石 on 14/12/24.
+//  Copyright (c) 2014年 骐俊通联. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
